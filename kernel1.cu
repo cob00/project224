c372d23f6627828a63202715e42c0db3e0c86ade
sandra-dergham <sdd03@mail.aub.edu>
2026-04-21 17:31:11 +0300

    kernel1: shared memory

--- kernel1.cu
+++ kernel1.cu
@@ -1,6 +1,135 @@
 #include "common.h"
+#include "matrix.h"
+
+__global__ void sptrsv_kernel1(
+    unsigned int  numRows,
+    unsigned int* rowPtrs,
+    unsigned int* colIdxs,
+    float*        matValues,
+    unsigned int* cscColPtrs,
+    unsigned int* cscRowIdxs,
+    float*        bValues,
+    float*        xValues,
+    unsigned int  numCols,
+    int*          dep_counter,
+    unsigned int* row_dep_count
+) {
+    unsigned int row = blockIdx.x;
+    unsigned int col = threadIdx.x;
+
+    if (row >= numRows || col >= numCols) return;
+
+    unsigned int rowStart = rowPtrs[row];
+    unsigned int rowEnd   = rowPtrs[row + 1];
+    unsigned int nnz      = rowEnd - rowStart;
+
+    extern __shared__ char smem[];
+    unsigned int* s_cols = (unsigned int*) smem;
+    float*        s_vals = (float*) (smem + nnz * sizeof(unsigned int));
+
+    for (unsigned int j = col; j < nnz; j += numCols) {
+        s_cols[j] = colIdxs[rowStart + j];
+        s_vals[j] = matValues[rowStart + j];
+    }
+
+    if (row > 0) {
+        while (atomicAdd(&dep_counter[row], 0) < (int)row_dep_count[row]) {
+            __nanosleep(10);
+        }
+    }
+    __threadfence();
+
+    __syncthreads();
+
+    float diag = 1.0f;
+    for (unsigned int j = 0; j < nnz; ++j) {
+        if (s_cols[j] == row) {
+            diag = s_vals[j] != 0.0f ? s_vals[j] : 1.0f;
+            break;
+        }
+    }
+
+    float sum = bValues[row * numCols + col];
+    for (unsigned int j = 0; j < nnz; ++j) {
+        unsigned int c = s_cols[j];
+        if (c < row) {
+            sum -= s_vals[j] * xValues[c * numCols + col];
+        }
+    }
+
+    xValues[row * numCols + col] = sum / diag;
+
+    __syncthreads();
+
+    if (col == 0) {
+        __threadfence();
+        unsigned int cStart = cscColPtrs[row];
+        unsigned int cEnd   = cscColPtrs[row + 1];
+        for (unsigned int j = cStart; j < cEnd; ++j) {
+            unsigned int dep = cscRowIdxs[j];
+            if (dep > row) {
+                atomicAdd(&dep_counter[dep], 1);
+            }
+        }
+    }
+}
 
 
-void sptrsv_gpu1(CSCMatrix* L_c, CSRMatrix* L_r, DenseMatrix* B, DenseMatrix* X, CSCMatrix* L_c_host, CSRMatrix* L_r_host, unsigned int numCols){
+void sptrsv_gpu1(CSCMatrix* L_c, CSRMatrix* L_r, DenseMatrix* B, DenseMatrix* X,
+                 CSCMatrix* L_c_host, CSRMatrix* L_r_host, unsigned int numCols)
+{
+    unsigned int n = L_r_host->numRows;
 
+    unsigned int* row_dep_count_h = (unsigned int*)calloc(n, sizeof(unsigned int));
+    for (unsigned int i = 0; i < n; ++i) {
+        unsigned int count = 0;
+        for (unsigned int j = L_r_host->rowPtrs[i]; j < L_r_host->rowPtrs[i + 1]; ++j) {
+            if (L_r_host->colIdxs[j] < i) count++;
+        }
+        row_dep_count_h[i] = count;
+    }
+
+    unsigned int* row_dep_count_d;
+    int*          dep_counter_d;
+    cudaMalloc(&row_dep_count_d, n * sizeof(unsigned int));
+    cudaMalloc(&dep_counter_d,   n * sizeof(int));
+    cudaMemcpy(row_dep_count_d, row_dep_count_h, n * sizeof(unsigned int), cudaMemcpyHostToDevice);
+    cudaMemset(dep_counter_d, 0, n * sizeof(int));
+    free(row_dep_count_h);
+
+    CSRMatrix csr_shadow;
+    cudaMemcpy(&csr_shadow, L_r, sizeof(CSRMatrix), cudaMemcpyDeviceToHost);
+    CSCMatrix csc_shadow;
+    cudaMemcpy(&csc_shadow, L_c, sizeof(CSCMatrix), cudaMemcpyDeviceToHost);
+    DenseMatrix b_shadow, x_shadow;
+    cudaMemcpy(&b_shadow, B, sizeof(DenseMatrix), cudaMemcpyDeviceToHost);
+    cudaMemcpy(&x_shadow, X, sizeof(DenseMatrix), cudaMemcpyDeviceToHost);
+
+    unsigned int maxNnz = 0;
+    for (unsigned int i = 0; i < n; ++i) {
+        unsigned int nnz = L_r_host->rowPtrs[i + 1] - L_r_host->rowPtrs[i];
+        if (nnz > maxNnz) maxNnz = nnz;
+    }
+
+    unsigned int smemSize = maxNnz * (sizeof(unsigned int) + sizeof(float));
+
+    dim3 grid(n);
+    dim3 block(numCols);
+
+    sptrsv_kernel1<<<grid, block, smemSize>>>(
+        n,
+        csr_shadow.rowPtrs,
+        csr_shadow.colIdxs,
+        csr_shadow.values,
+        csc_shadow.colPtrs,
+        csc_shadow.rowIdxs,
+        b_shadow.values,
+        x_shadow.values,
+        numCols,
+        dep_counter_d,
+        row_dep_count_d
+    );
+
+    cudaFree(dep_counter_d);
+    cudaFree(row_dep_count_d);
 }
